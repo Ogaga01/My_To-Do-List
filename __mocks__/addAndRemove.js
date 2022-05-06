2244bb9a8afeac8652e0675b94c788379dcd55ed
Ogaga <iyaraogaga69@gmail.com>
2022-05-06 15:31:36 +0100

    Fix linters errors

--- __mocks__/addAndRemove.js
+++ __mocks__/addAndRemove.js
@@ -1,4 +1,5 @@
 import 'jest-localstorage-mock';
+
 const jsdom = require('jsdom');
 
 const { JSDOM } = jsdom;
@@ -8,72 +9,68 @@ const dom = new JSDOM(`
 const { document } = (new JSDOM('...')).window;
 
 const checkItem = localStorage.getItem('TaskToday');
-let toDoList = [];
+const toDoList = [];
 
 class AllFunctions {
-
-    addR(addTask, id) {
-      
-        arr.forEach((element) => {
-          taskIndex = element.index;
-        });
-        if (addTask.length === 0) {
-          alert('it cannot be empty');
-          return arr.length;
-        }
-        const newTask = {
-          description: addTask,
-          completed: false,
-          index: id,
-        };
-        arr.push(newTask);
-        setStorage(arr);
-        return arr.length;
+  addR(addTask, id) {
+    arr.forEach((element) => {
+      taskIndex = element.index;
+    });
+    if (addTask.length === 0) {
+      alert('it cannot be empty');
+      return arr.length;
     }
+    const newTask = {
+      description: addTask,
+      completed: false,
+      index: id,
+    };
+    arr.push(newTask);
+    setStorage(arr);
+    return arr.length;
+  }
 
-    add(addTask, id) {
+  add(addTask, id) {
+    const newTask = {
+      description: addTask,
+      completed: false,
+      index: id,
+    };
+    if (addTask === '') {
+      return -1;
+    } if (!checkItem) {
+      toDoList.push(newTask);
+      localStorage.setItem('TaskToday', JSON.stringify(toDoList));
+      return toDoList.length;
+    }
+    const ArrayStored = localStorage.getItem('TaskToday');
+    const ArrayStoredParse = JSON.parse(ArrayStored);
+    ArrayStoredParse.push(newTask);
+    localStorage.setItem('TaskToday', JSON.stringify(ArrayStoredParse));
+    return ArrayStoredParse.length;
+  }
 
-        const newTask = {
-            description: addTask,
-            completed: false,
-            index: id,
-          };
-        if (addTask === '') {
-          return -1;
-        } if (!checkItem) {
-          toDoList.push(newTask);
-          localStorage.setItem('TaskToday', JSON.stringify(toDoList));
-          return toDoList.length;
-        }
-        const ArrayStored = localStorage.getItem('TaskToday');
-        const ArrayStoredParse = JSON.parse(ArrayStored);
-        ArrayStoredParse.push(newTask);
-        localStorage.setItem('TaskToday', JSON.stringify(ArrayStoredParse));
-        return ArrayStoredParse.length;
-      }
+  updateInput(description, id) {
+    const ArrayStored = localStorage.getItem('TaskToday');
+    const ArrayStoredParse = JSON.parse(ArrayStored);
+    if (ArrayStoredParse.length === 0 || ArrayStoredParse.length < id) {
+      return -1;
+    }
+    ArrayStoredParse.forEach((a, i) => {
+      ArrayStoredParse[id].description = description;
+      localStorage.setItem('TaskToday', JSON.stringify(ArrayStoredParse));
+    });
+    return JSON.parse(localStorage.getItem('TaskToday'))[id].description;
+  }
 
-      updateInput(description, id) {
-        const ArrayStored = localStorage.getItem('TaskToday');
-        const ArrayStoredParse = JSON.parse(ArrayStored);
-        if(ArrayStoredParse.length === 0 || ArrayStoredParse.length < id) {
-          return -1;
-        }else {
-          ArrayStoredParse.forEach((a, i) => {
-            ArrayStoredParse[id].description = description;
-            localStorage.setItem('TaskToday', JSON.stringify(ArrayStoredParse));
-          });
-          return JSON.parse(localStorage.getItem('TaskToday'))[id].description;
-        }
-      }  
-    
-    updateId() {
-        const TasksR = JSON.parse(localStorage.getItem('TaskToday'));
-        TasksR.forEach((a, i) => {
-          a.index = i;
-          localStorage.setItem('TaskToday', JSON.stringify(TasksR));
-        });
-      }
-    
+  updateId() {
+    const TasksR = JSON.parse(localStorage.getItem('TaskToday'));
+    TasksR.forEach((a, i) => {
+      a.index = i;
+      localStorage.setItem('TaskToday', JSON.stringify(TasksR));
+    });
+  }
+
   removeTask(id) {
     this.updateId();
     const BookStored = JSON.parse(localStorage.getItem('TaskToday'));
@@ -100,7 +97,7 @@ class AllFunctions {
     const BookFiltered = TasksR.filter((book) => book.completed !== true);
     localStorage.setItem('TaskToday', JSON.stringify(BookFiltered));
     return BookFiltered.length;
-  }  
+  }
 }
 
 export default AllFunctions;
